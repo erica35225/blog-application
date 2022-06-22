1211a6951aa302c4cb7904dec5df425272316cae
erica35225 <erica35225@gmail.com>
2022-06-22 22:44:26 +0530

    add getPost

--- src/main/java/com/blog/blogapplication/controllers/PostController.java
+++ src/main/java/com/blog/blogapplication/controllers/PostController.java
@@ -1,4 +1,17 @@
 package com.blog.blogapplication.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
 public class PostController {
+    @GetMapping("/post")
+    public List<Object> getPost(Long id){
+        return Collections.emptyList();
+    }
 }
